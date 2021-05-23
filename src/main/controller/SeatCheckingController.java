387726a3cc915d8429c4424b0a371b028bc30fd0
Nguyen Tuan Dat <s3726128@student.rmit.edu.au>
2021-05-23 10:56:04 +0700

    todo: fixing locked seat and booking seat

--- src/main/controller/SeatCheckingController.java
+++ src/main/controller/SeatCheckingController.java
@@ -1,7 +1,6 @@
 package main.controller;
 
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
@@ -12,11 +11,16 @@ import javafx.stage.Stage;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import main.SQLConnection;
 
 public class SeatCheckingController implements Initializable
 {
@@ -24,14 +28,134 @@ public class SeatCheckingController implements Initializable
     @FXML
     private Label txtSeat;
     @FXML
-    private Label txtAvailability;
-    @FXML
     private Button H1,H2,H3,H4,H5,H6,H7,V1,V2,V3,V4,V5,V6,V7,V8,V9;
+
+    private String [] seatNameArr = new String[16];
+    private Boolean[] avaiArr = new Boolean[16];
 
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
-       
+        try
+        {
+            seatCalling();
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    //checking which seat is available
+    public void seatCalling() throws SQLException
+    {
+        SQLConnection sqlConnection = new SQLConnection();
+        Connection connectionDB = sqlConnection.connect();
+
+        PreparedStatement preparedStatement = connectionDB.prepareStatement("select * from seat");
+        ResultSet rs = preparedStatement.executeQuery();
+
+        int inc = 0;
+        while (rs.next())
+        {
+            seatNameArr[inc] = rs.getString("seatname");
+            avaiArr[inc] = rs.getBoolean("availability");
+            inc++;
+        }
+    }
+
+    // check which seat is locked
+    /* public void lockedSeat()
+    {
+        for (int i = 0; i< seatNameArr.length; i++)
+        {
+            if (!avaiArr[i])
+            {
+                if (seatNameArr[i] == H1.getId())
+                {
+                    H1.setStyle("-fx-background-color: red");
+                }
+                else if (seatNameArr[i] == "H2")
+                {
+                    H2.setStyle("-fx-background-color: red");
+                }
+                if (seatNameArr[i] == "H3")
+                {
+                    H3.setStyle("-fx-background-color: red");
+                }
+            }
+        }
+    } */
+
+    // see which seat is clicked
+    public void handleButtonAction(ActionEvent e)
+    {
+        if (e.getSource() == H1)
+        {
+            lastClickedIndex = "H1";
+        }
+        else if (e.getSource() == H2)
+        {
+            lastClickedIndex = "H2";
+        }
+        else if (e.getSource() == H3)
+        {
+            lastClickedIndex = "H3";
+        }
+        else if (e.getSource() == H4)
+        {
+            lastClickedIndex = "H4";
+        }
+        else if (e.getSource() == H5)
+        {
+            lastClickedIndex = "H5";
+        }
+        else if (e.getSource() == H6)
+        {
+            lastClickedIndex = "H6";
+        }
+        else if (e.getSource() == H7)
+        {
+            lastClickedIndex = "H7";
+        }
+        else if (e.getSource() == V1)
+        {
+            lastClickedIndex = "V1";
+        }
+        else if (e.getSource() == V2)
+        {
+            lastClickedIndex = "V2";
+        }
+        else if (e.getSource() == V3)
+        {
+            lastClickedIndex = "V3";
+        }
+        else if (e.getSource() == V4)
+        {
+            lastClickedIndex = "V4";
+        }
+        else if (e.getSource() == V5)
+        {
+            lastClickedIndex = "V5";
+        }
+        else if (e.getSource() == V6)
+        {
+            lastClickedIndex = "V6";
+        }
+        else if (e.getSource() == V7)
+        {
+            lastClickedIndex = "V7";
+        }
+        else if (e.getSource() == V8)
+        {
+            lastClickedIndex = "V8";
+        }
+        else if (e.getSource() == V9)
+        {
+            lastClickedIndex = "V9";
+        }
+
+        txtSeat.setText(lastClickedIndex);
     }
 
     //cancel booking and return to homepage
